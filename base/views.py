5b05f9e3bd70713f2b13c5206f7f92a65f9ffc7a
Vladyslav Demenkov <Vladyslav Demenkov@jbw.awo>
2022-05-31 10:53:15 +0200

    commit 03

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 # Create your views here.
+
+def home(request):
+    return render(request, 'base/home.html')
+
+
+def testView(request):
+    return render(request, 'base/rooms.html')
