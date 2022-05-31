b09a655563d1efe24ea710a73039e774cf98121b
Vladyslav Demenkov <Vladyslav Demenkov@jbw.awo>
2022-05-31 14:30:43 +0200

    commit 05 with createRoom func

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name=''),
+    path('', views.home, name='home'),
     path('rooms/<str:pk>/', views.roomsView, name='rooms'),
+    path('create-room/', views.createRoom, name='create-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic
+from .forms import RoomForm
 # Create your views here.
 
 def home(request):
@@ -13,4 +14,20 @@ def roomsView(request, pk):
     rooms2 = Room.objects.get(id=pk)
     context = {'rooms2': rooms2}
     return render(request, 'base/rooms.html', context)
+
+
+#-------------------------------------------------Create room
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') #name in the URLs
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
 
